06faaaafb494e5d8423681054eabaa4aa8864360
Shashi Singh <singh.shashishekhar@gmail.com>
2015-10-28 01:32:12 +0530

    Shell script to pack extension

---
+++ packExtension.sh
@@ -0,0 +1,2 @@
+zip -r build/release/TweetSmartExt.zip background.js chrome_ex_oauth.js chrome_ex_oauth.html chrome_ex_oauthsimple.js LICENSE manifest.json onload.js popup.html bin/ bootstrap-3.3.4/ fonts/ css/ images/ less/ scss/
+
